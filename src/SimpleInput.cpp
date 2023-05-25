2654b3e15b01d1087c36e3b068639241c8a272ef
Emirhan Göktas <emirhangoktas01@gmail.com>
2023-05-25 14:28:07 +0200

    Refactor SimpleInput.cpp for cross-platform compatibility - Refactored SimpleInput.cpp for Linux and Windows - Updated getSteuerung, getWASD, getArrow functions - Improved cross-platform compatibility of the code

--- src/SimpleInput.cpp
+++ src/SimpleInput.cpp
@@ -9,20 +9,109 @@
 
 #include "SimpleInput.h"
 #include "Configuration.h"
-#include <windows.h>
 #include <iostream>
-//Microsoft Codetabelle
-//https://msdn.microsoft.com/de-de/library/windows/desktop/dd375731(v=vs.85).aspx
 
 SimpleInput::SimpleInput()
 {
 }
 
-
 SimpleInput::~SimpleInput()
 {
 }
+#if defined(__linux__) //Linuxvariante
+#include <termios.h> //Terminalkontrollfunktionen (Puffer, Verz�gerung, Echo, etc.)
+#include <unistd.h>  //Stellt unter anderem sleep und usleep bereit
+#include <fcntl.h>	 //Unix Systemcalls (Filedescriptor) Lese-/Schreibfunktionalit�t
+#include <chrono>	 //Zeitumrechnungsfunktionen und exakte Zeitnahme
 
+//Funktion pr�ft auf Tastatureingabe f�r die Dauer des Intervalls in ms
+char SimpleInput::getSteuerung(long interval)
+{
+	//Hilfsvariablen zur Berechnung der Rechenzeit der Funktion
+	auto startzeit = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>
+		(std::chrono::high_resolution_clock::now() - startzeit);
+
+	struct termios tio, oldtio;
+	tcgetattr(STDIN_FILENO, &oldtio); 		// Sichern der aktuellen Terminaleinstellung
+	oldtio.c_lflag &= ~(ECHO);				// Echo f�r Eingaben dauerhaft auf false setzen.
+	tio = oldtio;
+	tio.c_lflag &= ~(ICANON | ECHO); 		// Verz�gerungsfreies Einlesen ohne Ausgabe auf der Konsole
+	tio.c_cc[VMIN] = 0; 					// Minimale Anzahl an Zeichen pro Lesevorgang
+	tio.c_cc[VTIME] = 0; 					// Timeout bei Verwendung von Select in 1/10s
+	tcsetattr(STDIN_FILENO, TCSANOW, &tio); // Setzen der neuen Terminaleinstellung
+	//Puffer leeren
+	char dump;
+	while (read(STDIN_FILENO, &dump, 1) > 0);
+
+	//Wartet bis zur Restdauer in ms auf Tastatureingaben. Abtastrate 1000Hz
+	char c = 'x';
+	while (duration.count() < interval)
+	{
+		if (read(STDIN_FILENO, &c, 1) == 1)
+		{
+			//std::cout << "Eingabe: " << c << std::endl;
+			break;
+		}
+		usleep(1); // Reduziert die CPU-Belastung durch Reduzierung der Abtastrate
+		//Ermitteln der Restzeit f�r das Warten auf Tastatureingaben
+		duration = std::chrono::duration_cast<std::chrono::milliseconds>
+			(std::chrono::high_resolution_clock::now() - startzeit);
+	}
+	// Zur�cksetzen der Terminaleinstellung
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldtio); 		
+
+	duration = std::chrono::duration_cast<std::chrono::milliseconds>
+		(std::chrono::high_resolution_clock::now() - startzeit);
+	//Berechnen der verbleibenden Restzeit in ms
+	long dauer = 0;
+	if (interval - duration.count() > 0)
+		dauer = (interval - duration.count()) * 1000;
+	//Mainthread pausiert f�r die Dauer der Restzeit
+	usleep(dauer);
+	return c;
+}
+
+char SimpleInput::getWASD(long interval)
+{
+	char c = getSteuerung(interval);
+
+	if (c == 'w')
+		return Configuration::GAMEOBJECT_MOVE_UP;// 'w';
+	else if (c == 'a')
+		return Configuration::GAMEOBJECT_MOVE_LEFT;// 'a';
+	else if (c == 's')
+		return Configuration::GAMEOBJECT_MOVE_DOWN;// 's';
+	else if (c == 'd')
+		return Configuration::GAMEOBJECT_MOVE_RIGHT;// 'd';
+	else if (c == 'e')
+		return Configuration::GAMEOBJECT_ACTION;// 'e';
+	else
+		return 'x';
+}
+
+char SimpleInput::getArrow(long interval)
+{
+	char c = getSteuerung(interval);
+
+	if (c == '8')
+		return Configuration::GAMEOBJECT_MOVE_UP;// '8';
+	else if (c == '4')
+		return Configuration::GAMEOBJECT_MOVE_LEFT;// '4';
+	else if (c == '5')
+		return Configuration::GAMEOBJECT_MOVE_DOWN;// '5';
+	else if (c == '6')
+		return Configuration::GAMEOBJECT_MOVE_RIGHT;// '6';
+	else if (c == '7')
+		return Configuration::GAMEOBJECT_ACTION;// '7';
+	else
+		return 'x';
+}
+
+#elif defined(_WIN32) //Greift bei Windows 32-bit und 64-bit
+//Windows Virtual Key Codes => https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+
+#include <windows.h>
 char SimpleInput::getSteuerung(long interval)
 {
 	Sleep(interval);
@@ -76,8 +165,26 @@ char SimpleInput::getArrow(long interval)
 	else if ((GetAsyncKeyState(0x27) & KEY_UP) == KEY_UP)
 		return Configuration::GAMEOBJECT_MOVE_RIGHT;// '6';
 	//Enter
-	else if ((GetAsyncKeyState(0x0D) & KEY_UP) == KEY_UP)
+	else if ((GetAsyncKeyState(0x0D) & KEY_UP) == KEY_UP) //Enter Taste
 		return Configuration::GAMEOBJECT_ACTION;// 'e';
 	else
 		return 'x';
 }
+
+#else //Betriebssystem unbekannt / andere Betriebssysteme
+char SimpleInput::getSteuerung(long interval)
+{
+	char eingabe;
+	std::cin >> eingabe;
+	return eingabe;
+}
+char SimpleInput::getWASD(long interval)
+{
+	return getSteuerung();
+}
+char SimpleInput::getArrow(long interval)
+{
+	return getSteuerung();
+}
+
+#endif
